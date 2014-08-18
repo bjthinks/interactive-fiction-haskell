bbc3aba1ffc75b1197faa30717c69801256556b1
Brian Johnson (Linux Desktop) <bjthinks@gmail.com>
2014-08-18 00:31:49 -0700

    Read a line using Haskeline

--- fta.hs
+++ fta.hs
@@ -1,1 +1,8 @@
-main = return ()
+import System.Console.Haskeline
+import Control.Monad.Trans
+
+main :: IO ()
+main = runInputT defaultSettings $ do
+  x <- getInputLine "> "
+  lift $ print x
+  return ()
