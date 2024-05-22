cc6410948e3406071b234b2f90c3e6ef014d9a79
Brian Johnson <bjthinks@gmail.com>
2024-05-22 15:23:57 -0700

    Slightly expand game world with two rooms and exits

--- BuildWorld.hs
+++ BuildWorld.hs
@@ -6,9 +6,12 @@ import WorldOps
 buildWorld :: GameMonad ()
 buildWorld = do
 
-  house <- newRoom "House" "This is your house. It looks pretty ordinary."
+  house <- newRoom "Haunted House" "This house looks very spooky. The windows are boarded up and the green paint is peeling."
+  brisbin <- newRoom "Brisbin Street" "You are in the middle of Brisbin Street. The street continues to the west and east. To the north is a haunted house, and to the south is Ray\'s house."
+  newExit "north" brisbin house
+  newExit "south" house brisbin
 
-  player <- newObject "Player" "You look normal." house
+  player <- newObject "Player" "You look normal." brisbin
   setPlayer player
 
   newObject "Blet" "It's a blet. It defies description." house
