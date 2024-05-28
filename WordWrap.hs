9500581d1d2c209fe63bea6e80f61f01c3870466
Brian Johnson <bjthinks@gmail.com>
2024-05-28 10:44:39 -0700

    Slightly less BS definition of wordWrap and use it

--- WordWrap.hs
+++ WordWrap.hs
@@ -1,6 +1,11 @@
 module WordWrap(wordWrap) where
 
-wordWrap "" = ""
-wordWrap str
-  | length str <= 70 = str
-  | otherwise = take 70 str ++ "\n" ++ wordWrap (drop 70 str)
+wordWrap :: String -> String
+wordWrap str = wrap "" 0 str
+
+wrap :: String -> Int -> String -> String
+wrap output outputLen "" = reverse output
+wrap output outputLen ('\n':input) = wrap ('\n':output) 0 input
+wrap output outputLen (i:input)
+  | outputLen >= 70 = wrap ('\n':output) 0 (i:input)
+  | otherwise       = wrap (i:output) (outputLen+1) input
--- ifh.hs
+++ ifh.hs
@@ -7,17 +7,18 @@ import Control.Monad.RWS
 import Defs
 import BuildWorld
 import Verbs
+import WordWrap
 
 mainloop :: GameState -> MaybeT (InputT IO) ()
 mainloop state = do
   line <- MaybeT $ getInputLine "> "
   let (newState, response) = execRWS handleInput line state
-  liftIO $ putStr response
+  liftIO $ putStr $ wordWrap response
   mainloop newState
 
 main = do
   let (state, _) = execRWS buildWorld "" startState
   let (state', response) = execRWS handleInput "look" state
-  liftIO $ putStr response
+  liftIO $ putStr $ wordWrap response
   putStrLn "Type help for a list of commands."
   runInputT defaultSettings $ runMaybeT $ mainloop state'
