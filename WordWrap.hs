4616b294e7558f1f215e3e200b91178c3fddf05e
Brian Johnson <bjthinks@gmail.com>
2024-05-28 13:54:35 -0700

    No more magic number 70 for maxLineLen

--- WordWrap.hs
+++ WordWrap.hs
@@ -3,9 +3,12 @@ module WordWrap(wordWrap) where
 wordWrap :: String -> String
 wordWrap str = wrap "" 0 str
 
+maxLineLen :: Int
+maxLineLen = 70
+
 wrap :: String -> Int -> String -> String
 wrap output outputLen "" = reverse output
 wrap output outputLen ('\n':input) = wrap ('\n':output) 0 input
 wrap output outputLen (i:input)
-  | outputLen >= 70 = wrap ('\n':output) 0 (i:input)
-  | otherwise       = wrap (i:output) (outputLen+1) input
+  | outputLen >= maxLineLen = wrap ('\n':output) 0 (i:input)
+  | otherwise               = wrap (i:output) (outputLen+1) input
