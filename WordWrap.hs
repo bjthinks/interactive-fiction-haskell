0d35209f51fb0b3ec90c966ec38be4e68dfd414d
Brian Johnson <bjthinks@gmail.com>
2024-06-07 03:24:15 -0700

    Chamge maxLineLen to lineLength

--- WordWrap.hs
+++ WordWrap.hs
@@ -1,21 +1,21 @@
 module WordWrap(wordWrap) where
 
-maxLineLen :: Int
-maxLineLen = 75
+lineLength :: Int
+lineLength = 75
 
 wordWrap :: String -> String
 wordWrap str = wrap "" 0 "" 0 str
 
 wrap :: String -> Int -> String -> Int -> String -> String
 wrap output outputLen partial partialLen input
-  | outputLen == 0 && partialLen >= maxLineLen && input /= "" &&
+  | outputLen == 0 && partialLen >= lineLength && input /= "" &&
     head input /= '\n' && head input /= ' ' =
       wrap ('\n' : partial ++ output) 0 "" 0 input
   | input == "" = reverse (partial ++ output)
   | head input == ' ' =
     wrap (' ' : (partial ++ output)) (partialLen + outputLen + 1) "" 0
     (tail input)
-  | outputLen + partialLen > maxLineLen =
+  | outputLen + partialLen > lineLength =
     wrap ('\n' : output) 0 "" 0 (reverse partial ++ input)
   | head input == '\n' =
     wrap ('\n' : (partial ++ output)) 0 "" 0 (tail input)
