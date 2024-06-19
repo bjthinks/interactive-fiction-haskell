397b7ba4ad64383d20745e191b775a8dbb75d071
Brian Johnson <bjthinks@gmail.com>
2024-06-19 15:02:27 -0700

    Remove "filter notArticle" from parser

--- ParseInput.hs
+++ ParseInput.hs
@@ -163,13 +163,7 @@ parseInput names input = runParser parseLine sortedNames "" inputWithPos
   where
     sortedNames = longestFirst $ tokenizeNames names
     longestFirst = sortOn (negate . length . fst)
-    inputWithPos = zipWith (,) [1..] $ filter notArticle $ words input
-
-notArticle :: String -> Bool
-notArticle "a" = False
-notArticle "an" = False
-notArticle "the" = False
-notArticle _ = True
+    inputWithPos = zipWith (,) [1..] $ words input
 
 tokenizeNames :: [(String,Ref)] -> [([Word],Ref)]
 tokenizeNames = map wordizeName
