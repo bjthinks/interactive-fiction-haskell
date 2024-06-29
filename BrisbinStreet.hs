36bf90db7ad677286c1a6a77ad223bdf78747711
Brian Johnson <bjthinks@gmail.com>
2024-06-29 15:02:58 -0700

    Add comment

--- BrisbinStreet.hs
+++ BrisbinStreet.hs
@@ -50,7 +50,7 @@ buildWorld = do
   newExit "south" frontYard brisbin
   acorns <- newObject frontYard "acorns" $
     "Ordinary white oak acorns. Could you throw them at a squirrel?"
-  addAlias acorns "acorn"
+  addAlias acorns "acorn" -- TODO "an acorn"
   setVerb1 "eat" acorns $
     msg $ "You try one, but they taste terribly bitter. Maybe a squirrel " ++
       "would like them if you threw them at it?"
