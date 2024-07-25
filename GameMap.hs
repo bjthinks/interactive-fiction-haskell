4d59493ef6fa9aa1da86a65983615ea40d8273f8
Brian Johnson <bjthinks@gmail.com>
2024-07-25 15:57:20 -0700

    Add parens

--- GameMap.hs
+++ GameMap.hs
@@ -18,7 +18,7 @@ printMap = do
     y <- [ymax,ymax-1..ymin]
     x <- [xmin..xmax]
     (if x == xmin then " " else "") ++ [m ! (x,y)] ++
-      if x == xmax then "\n" else ""
+      (if x == xmax then "\n" else "")
   msg ""
 
 testMap :: GameMap
