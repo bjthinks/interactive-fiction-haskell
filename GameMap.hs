f7db878c2112beaa3c314fdf3c190574ef5e047f
Brian Johnson <bjthinks@gmail.com>
2024-07-25 14:14:28 -0700

    Start working on printMap

--- GameMap.hs
+++ GameMap.hs
@@ -1,2 +1,13 @@
 module GameMap where
 
+import Control.Monad
+import Data.Maybe
+import Defs
+
+printMap :: Game ()
+printMap = do
+  let region = 1
+  mm <- getMap region
+  when (isNothing mm) $ stop "There is no map for this area."
+  let _ = fromJust mm
+  return ()
